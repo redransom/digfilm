667e74a96213e5e82ef871966d60644da873a84e
Kinsley Ransom <kinsley@redransom.co.uk>
2016-07-19 21:33:11 +0100

    Roster image fix

--- resources/views/partials/user-roster.blade.php
+++ resources/views/partials/user-roster.blade.php
@@ -1,8 +1,17 @@
 @if($currentLeague->rosters()->count() > 0)
+    <style>
+        .feature-table img {
+            max-width: 58px !important;
+            max-height: 80px !important;
+        }
 
+        .feature-table td {
+            font-size: 0.8em;
+        }
+    </style>
 <table class="feature-table dark-gray">
     <thead>
-        <tr><th width="40%">Movie</th><th width="15%">Date</th><th width="15%">Amount Bid</th><th width="15%">Total Gross</th><th width="15%">Value For Money</th></tr>
+        <tr><th width="15%">&nbsp;</th><th width="40%">Movie</th><th width="15%">Date</th><th width="15%">Amount Bid</th><th width="15%">Total Gross</th><th width="15%">Value For Money</th></tr>
     </thead>
     <tbody>
     <?php
@@ -14,7 +23,13 @@
         $total_gross += $roster_line->total_gross;
         $bid += $roster_line->bid_amount;
         ?>
-        <tr><td>
+        <tr>
+        @if(!is_null($roster_line->movie->firstImage()))
+        <td><img src='{{asset($roster_line->movie->firstImage()->file_name)}}'/></td>
+        @else
+        <td><img src="{{asset('images/TNBF.jpg')}}"/></td>
+        @endif
+        <td>
         @if(is_null($roster_line->movie->slug) || $roster_line->movie->slug == '')
         <a href="{{URL('movie-knowledge', [$roster_line->movie->id])}}">
         @else
@@ -27,7 +42,7 @@
         </tr>
     @endforeach
         <?php $vfm = ($bid != 0) ? (($total_gross / $bid) / 100000) : 0;?>
-        <tr><td colspan="2">Totals</td><td align="right"><strong>${{number_format($bid, 2)}}</strong></td>
+        <tr><td colspan="3">Totals</td><td align="right"><strong>${{number_format($bid, 2)}}</strong></td>
         <td align="right"><strong>${{number_format($total_gross, 0, ".", ",")}}</strong></td><td align="right"><strong>{{number_format($vfm, 2)}}</strong></td></tr>        
     </tbody>
 </table>
