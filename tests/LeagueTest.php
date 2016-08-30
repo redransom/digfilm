d0326240dbae27bad7a7e71a42e8e18c619f045b
redransom <kinsleyr@redransom.local>
2016-08-30 16:06:59 +0100

    Test controller actions in league

--- tests/LeagueTest.php
+++ tests/LeagueTest.php
@@ -1,4 +1,5 @@
 <?php
+use App\Models\Movie;
 
 class LeagueTest extends TestCase {
 
@@ -11,7 +12,16 @@ class LeagueTest extends TestCase {
 	{
         //$this->assetEquals(1, 1);
         //$this->assetEquals(2, 1);
-		$response = $this->action('POST', 'LeagueController@store');
+		//$response = $this->action('POST', 'LeagueController@store');
+
+		$response = $this->action('POST', 'LeaguesController@postInvitePlayer', 
+			['leagues_id'=>'57', 
+			'name'=>[0=>'john', 1 => 'mike'], 
+			'email_address'=>[0=>'johntest@gmail.com', 1=>'miketest@gmail.com']]);
+
+		$movie = Movie::first();
+		$response = $this->action('POST', 'LeaguesController@postMovie',
+			['leagues_id'=>57, 'movies_id'=>$movie->id]);
 	}
 
 }
